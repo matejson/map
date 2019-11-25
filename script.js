2aac0eac87336b240a3f9d15621f46f930000d58
Matejson <mateusz.zaborowskii@gmail.com>
2019-11-25 01:04:59 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+$('.owl-carousel').owlCarousel({
+    items:1,
+    margin:20,
+    lazyLoad: true,
+    smartSpeed:450,
+    loop: true,
+ 
+        
+});
+});
+
+tippy('path')
+
